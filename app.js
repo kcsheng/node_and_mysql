7c3ba1175cf63055a616417942a65170fb567c6c
kcsheng <kacysheng@mail.com>
2021-05-16 19:51:29 +1000

    Establish sql connection within node and create a database via a route

--- app.js
+++ app.js
@@ -1,7 +1,36 @@
-let faker = require('faker');
-email1 = faker.internet.email();
-date1 = faker.date.past();
-city1 = faker.address.city();
-console.log(email1);
-console.log(date1);
-console.log(city1);
+const mysql = require('mysql');
+const express = require('express');
+const faker = require('faker');
+
+const db = mysql.createConnection({
+  host     : '192.168.64.2',
+  user     : 'root',
+  password : '12345',
+  database : 'join_us'
+});
+
+db.connect((err) => {
+  if(err) { 
+    throw err; 
+  }
+  console.log('MySql is conneted...');
+});
+
+const app = express();
+
+app.get('/createdb', (request, response) => {
+  let sql = 'CREATE DATABASE join_us';
+  db.query(sql, (err, result) => {
+    if(err) {
+      throw err
+    }
+    console.log(result);
+    response.send('database created...');
+  })
+})
+
+// app.get('/createtable', (request, response))
+
+app.listen('3000', () => {
+  console.log('Server started on port 3000');
+})
